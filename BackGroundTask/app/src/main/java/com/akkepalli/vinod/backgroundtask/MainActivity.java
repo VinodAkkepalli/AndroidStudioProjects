63495605bc4c25b1ea917ea121fd29f710157eb4
VinodAkkepalli <shining.vinod@gmail.com>
2015-09-26 00:47:32 +0530

    Practice AsyncTask and ProgressBar update project
    Asynctask performs background task, and displays progress updates on the
    activity

---
+++ BackGroundTask/app/src/main/java/com/akkepalli/vinod/backgroundtask/MainActivity.java
@@ -0,0 +1,71 @@
+package com.akkepalli.vinod.backgroundtask;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    private ProgressBar mProgress;
+    private int mProgressStatus = 0;
+    TextView percentage = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mProgress = (ProgressBar) findViewById(R.id.progress_bar);
+
+        percentage = (TextView) findViewById(R.id.percentage);
+
+        new CountProgress().execute();
+
+    }
+
+
+    class CountProgress extends AsyncTask<Void, Integer, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            mProgress.setProgress(0);
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... unused) {
+            for (int i=0; i<101;i++ ) {
+                if (isCancelled())
+                    break;
+
+                publishProgress(i);
+                SystemClock.sleep(200);
+            }
+
+            return(null);
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... progress) {
+            if (!isCancelled()) {
+
+                mProgress.setVisibility(View.VISIBLE);
+
+                // updating progress bar value
+                mProgress.setProgress(progress[0]);
+                // updating progess percentage text
+                percentage.setText(progress[0].toString() + "%");
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            Toast.makeText(getApplicationContext(), R.string.done, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
